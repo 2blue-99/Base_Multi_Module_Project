3755fbc91bb555d586b319d5c2029a7119f1cc33
pureum <lpm0831@naver.com>
2025-11-24 22:43:46 +0900

    JVM Library Convention Plugin

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,11 +1,7 @@
 plugins {
-    id("java-library")
     alias(libs.plugins.multi.module.jvm.library)
 }
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-}
+
 kotlin {
     compilerOptions {
         jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
